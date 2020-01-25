5a68dae92182cad9bbeac4351988a9bdfa977353
Stefan Bischof <stbischof@bipolis.org>
2020-01-25 10:25:12 +0100

    add dto to graal_class_config remove scr sysouts

--- atomos.tests/service.substrate/src/main/java/org/atomos/service/substrate/GogoConsole.java
+++ atomos.tests/service.substrate/src/main/java/org/atomos/service/substrate/GogoConsole.java
@@ -22,8 +22,6 @@ import org.atomos.service.contract.Echo;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.launch.Framework;
-import org.osgi.service.component.runtime.ServiceComponentRuntime;
-import org.osgi.service.component.runtime.dto.ComponentDescriptionDTO;
 import org.osgi.service.log.LogEntry;
 import org.osgi.service.log.LogLevel;
 import org.osgi.service.log.LogReaderService;
@@ -59,15 +57,6 @@ public class GogoConsole
             Echo echo = bc.getService(bc.getServiceReference(Echo.class));
             String result = echo.echo("Echo works!");
             System.out.println(result);
-
-            ServiceComponentRuntime scr = bc.getService(
-                bc.getServiceReference(ServiceComponentRuntime.class));
-            ComponentDescriptionDTO[] scrResult = scr.getComponentDescriptionDTOs().toArray(
-                new ComponentDescriptionDTO[0]);
-          for (ComponentDescriptionDTO componentDescriptionDTO : scrResult)
-        {
-            System.out.println(componentDescriptionDTO);
-        }
         }
         catch (Exception e2)
         {
